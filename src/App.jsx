b0f717b13a3bec7446b1716c35b535b2f860d3de
haydenshubert <hshubert05@gmail.com>
2025-11-10 20:19:05 -0600

    App routing and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import Home from './pages/Home';
 import Test from './pages/Test';
 import Prompt from './pages/Prompt';
 
 function App() {
   return (
-    <div>
-      <Prompt />
-    </div>
+    <Router>
+      <nav className="flex place-content-center">
+        <Link to="/test" className="m-1.5 border hover:bg-green-300">
+          Test
+        </Link>
+        <Link to="/prompt" className="m-1.5 border hover:bg-green-300">
+          Prompt
+        </Link>
+      </nav>
+
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/test" element={<Test />} />
+        <Route path="/prompt" element={<Prompt />} />
+      </Routes>
+    </Router>
   );
 }
 
