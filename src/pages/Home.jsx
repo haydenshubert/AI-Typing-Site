f96e61734d0cb137dbeb583c1d57b1aa999b4599
haydenshubert <hshubert05@gmail.com>
2025-11-24 20:19:24 -0600

    Near finished product, still some CSS changes to be made

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,31 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function Home() {
-  return <div></div>;
-}
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex min-h-screen flex-col items-center bg-gray-200">
+      <p className="text-center text-5xl font-bold">Welcome to Prompt2Type!</p>
+      <section className=" ">
+        <p className="itmes-left mt-20 mr-20 text-3xl font-bold text-blue-900">
+          Introduction:
+        </p>
+        <p className="mt-4 w-150 rounded-lg border-2 bg-gray-300 p-2 text-2xl text-wrap">
+          This site lets the user type about anything they want. All you have to
+          do is give a short prompt and then wait a few seconds and a paragraph
+          tailored to your prompt will appear. You can type about whatever comes
+          to mind or get a random prompt if you want.
+        </p>
+      </section>
+      <button
+        onClick={() => navigate('/prompt')}
+        className="mt-16 w-30 rounded-lg border-2 border-gray-800 bg-blue-500 p-0.5 text-xl font-bold text-white hover:bg-blue-400"
+      >
+        Get Started
+      </button>
+    </div>
+  );
+};
 
 export default Home;
--- src/pages/Prompt.jsx
+++ src/pages/Prompt.jsx
@@ -25,7 +25,7 @@ const Prompt = () => {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center bg-gray-200 pt-24">
+    <div className="relative flex min-h-screen flex-col items-center bg-gray-200 pt-24">
       {!loading ? (
         <>
           <p className="p-2 text-3xl">
@@ -47,6 +47,28 @@ const Prompt = () => {
           >
             Send Prompt
           </button>
+          <section className="absolute top-120 left-6 h-75 rounded-4xl border-2 p-2.5">
+            <p className="border-b">Story Prompt Example:</p>
+            <p className="mt-4">"Story about Donkey Kong as president"</p>
+            <p className="mt-2 w-80">
+              Donkey Kong, the former nemesis of Mario, stood confidently in the
+              Oval Office as the newly elected President of the United States.
+              His rugged, imposing presence commanded attention from the crowded
+              press corps gathered outside the White House. Despite his
+              unorthodox rise to power, Kong had won over voters with his...
+            </p>
+          </section>
+          <section className="absolute top-120 right-6 h-75 rounded-4xl border-2 p-2.5">
+            <p className="border-b">No Prompt Example:</p>
+            <p className="mt-6 w-80">
+              The human brain is a complex and fascinating organ that continues
+              to puzzle scientists and philosophers alike with its intricacies.
+              One of the most intriguing aspects of the brain is its ability to
+              reorganize itself in response to new experiences and environments.
+              This process, known as neuroplasticity, allows the brain to adapt
+              and change throughout...
+            </p>
+          </section>
         </>
       ) : (
         <div className="flex flex-col items-center">
